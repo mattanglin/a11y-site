10b040c8ae6d5adb195d38c82dfbbeca7bc6f182
Matt Anglin <manglin@audioeye.com>
2020-12-04 10:42:38 -0800

    2 pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
+import React from 'react';
+import { Router, Link } from '@reach/router';
 import logo from './logo.svg';
 import './App.css';
+
+const Home = () => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+    </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+    <Link to="/a11y">a11y</Link>
+  </header>
+);
+
+const A11y = () => (
+  <iframe
+    src="https://dev-portal.audioeye.com/accessibility-statement.html?domain=http://a11y.matthewanglin.com/"
+    title="Accessibility Statement"
+    style="width:100%;height:4700px"
+    frameborder="0"
+  />
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Home path="/" />
+        <A11y path="/a11y" />
+      </Router>
     </div>
   );
 }
